2b090ad6c0a3827634c5dc158809302259f3708a
kstover <kstover@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-29 17:34:02 +0000

    Version 1.2.7 Fixes a bug created by the coder's inability to type properly.
    git-svn-id: https://plugins.svn.wordpress.org/ninja-forms/tags/1.2.7@481944 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/body_fields.php
@@ -0,0 +1,110 @@
+<?php
+global $wp_editor, $wp_version;
+wp_nonce_field('ninja_save_form_fields','ninja_form_fields'); 
+$action = esc_html($_REQUEST['action']);
+?>	
+<input type="hidden" id="ninja_form_action" name="action" value="<?php echo $action;?>">
+<input type="hidden" name="submitted" value="yes">
+<input type="hidden" name="tab" value="fields">
+<input type="hidden" id="ninja_form_new" value="<?php echo $form_new;?>">
+<input type="hidden" id="ninja_form_id" name="ninja_form_id" value="<?php echo $form_id;?>">
+<input type="hidden" name="ninja_form_fields_order" id="ninja_form_fields_order" value="same">
+	<div id="nav-menu-header">
+		<div id="submitpost" class="submitbox">
+			<div class="major-publishing-actions">
+				<!--
+				<label class="menu-name-label howto open-label" for="menu-name">
+					<span><?php _e('Form Name'); ?></span>
+					<input name="menu-name" id="menu-name" type="text" class="menu-name regular-text menu-item-textbox input-with-default-title" title="<?php esc_attr_e('Enter Form Name Here'); ?>" value="TeSt" />
+				</label>
+				<br class="clear" />
+				-->
+					
+				<div class="publishing-action">
+					<input class="button-primary menu-save ninja_save_data" name="save_menu" id="ninja_save_data_top" type="submit" value="<?php esc_attr_e('Save Fields'); ?>" />
+				</div><!-- END .publishing-action -->
+				<!--
+				<div class="delete-action">
+					<a class="submitdelete deletion menu-delete" href="<?php echo esc_url( wp_nonce_url( admin_url('nav-menus.php?action=delete&amp;menu=' . $nav_menu_selected_id), 'delete-nav_menu-' . $nav_menu_selected_id ) ); ?>"><?php _e('Delete Menu'); ?></a>
+				</div><!-- END .delete-action -->
+				
+			</div><!-- END .major-publishing-actions -->
+		</div><!-- END #submitpost .submitbox -->
+		
+	</div><!-- END #nav-menu-header -->
+	<div id="post-body">
+		<div id="post-body-content">
+		<ul class="menu" id="ninja_edit_form_fields">
+
+			<?php
+			foreach($ninja_forms_fields as $field){
+				$id = $field['id'];
+				ninja_form_field_editor($id, false);
+			}
+			?>
+			
+			</ul>
+		</div><!-- /#post-body-content -->
+	</div><!-- /#post-body -->
+	<div id="nav-menu-footer">
+	<div class="major-publishing-actions">
+				<!----<label class="menu-name-label howto open-label">
+					<span><?php _e('Submit Button Text'); ?></span>
+					<input name="menu-name" id="menu-name" type="text" class="menu-name regular-text menu-item-textbox input-with-default-title" title="<?php esc_attr_e('Enter Form Name Here'); ?>" value="Submit" />
+				</label>
+				---->
+				<div class="publishing-action">
+					<input class="button-primary menu-save ninja_save_data" name="save_menu" id="ninja_save_data_bot" type="submit" value="<?php esc_attr_e('Save Fields'); ?>" />
+				</div><!-- END .publishing-action -->
+	</div><!-- END #nav-menu-header -->
+	</div>
+</form><!-- /#update-nav-menu -->
+<div id="editor_cont" style="display:none;">
+	<?php
+	if(version_compare( $wp_version, '3.3-beta3-19254' , '<')){
+		echo $wp_editor->editor($value, "hidden_editor", array('media_buttons_context' => '<span>Insert a media file: </span>', 'upload_link_title' => 'Media Uploader - NinjaForms'), true);
+	}else{
+		$args = array("media_buttons" => true);
+		wp_editor("", "hidden_editor" , $args);
+	}
+	?>
+</div>
+<div id="mask_help" class="ninja_help_text" title="Custom Mask Help" style="display:none;">
+		<p>Any character you place in the "custom mask" box that is not in the list below will be automatically entered for the user as they type and will not be removeable..</p>
+		</p>These are the predefined masking characters:
+			<ul>
+				<li>a - Represents an alpha character (A-Z,a-z) - Only allows letters to be entered.</lid>
+				<li>9 - Represents a numeric character (0-9) - Only allows numbers to be entered.</li>
+				<li>* - Represents an alphanumeric character (A-Z,a-z,0-9) - This allows both numbers and letters to be entered.</li>
+			</ul>
+		</p>
+		<p>
+			So, if you wanted to a mask for an an American Social Security Number, you'd put 999-99-9999 into the box. The 9's would represent any number, and the -'s would be automatically added. This would prevent the user from putting in anything other than numbers.
+		</p>
+		<p>
+			You can also combine these for specific applications. For instance, if you had a product key that was in the form of A4B51.989.B.43C, you could mask it with: a9a99.999.a.99a, which would force all the a's to be letters and the 9's to be numbers.
+		</p>
+</div>
+<div id="rename_help" class="ninja_help_text" title="File Renaming Help" style="display:none;">
+		<p>If you leave the advanced rename box empty, the uploaded file will retain the original user's filename. (With any special characters removed.)</p>
+		</p>If you want to rename the file, however, you can. These are the conventions that NinjaForms understands, and their effect.
+			<ul>
+				<li><span class="code">%filename%</span> - The file's original filename, with any special characters removed.</lid>
+				<li><span class="code">%formtitle%</span> - The title of the current form, with any special characters removed.</lid>
+				<li><span class="code">%username%</span> - The WordPress username for the user, if they are logged in.</lid>
+				<li><span class="code">%date%</span> - Today's date in yyyy-mm-dd format.</lid>
+				<li><span class="code">%month%</span> - Today's month in mm format.</lid>
+				<li><span class="code">%day%</span> - Today's day in dd format.</lid>
+				<li><span class="code">%year%</span> - Today's year in yyyy format.</lid>
+			</ul>
+		</p>
+		<p>
+			Any characters other than letters, numbers, dashes (-) and those on the list above will be removed. This includes spaces.
+		</p>
+		<p>
+			An Example: <span class="code">%date%-%filename%</span>
+		</p>
+		<p>
+			Would Yield: <span class="code">2011-07-09-myflowers.jpg</span>
+		<p>	
+</div>
---
+++ includes/scripts_functions.php
@@ -0,0 +1,155 @@
+<?php
+//Load up our WP Ninja Custom Form JS files.
+function ninja_form_admin_css(){
+	global $wp_version;
+	if(isset($_REQUEST['tab'])){
+		$tab = esc_html($_REQUEST['tab']);
+	}else{
+		$tab = '';
+	}
+	if($tab == 'preview'){
+		$plugin_settings = get_option("ninja_forms_settings");
+		$default_style = $plugin_settings['default_style'];
+		if($default_style == 'checked'){
+			wp_enqueue_style( 'ninja_forms_display_css', NINJA_FORMS_URL .'/css/ninja_forms_display.css');
+			wp_enqueue_style( 'jquery-smoothness-css', NINJA_FORMS_URL .'/css/smoothness/jquery-smoothness.css');
+		}
+	}
+	if($tab == 'subs'){
+		wp_enqueue_style( 'jquery-tablesorter-css', NINJA_FORMS_URL .'/css/tablesorter-style.css');
+	}
+	wp_enqueue_style( 'jquery-smoothness-css', NINJA_FORMS_URL .'/css/smoothness/jquery-smoothness.css');
+	wp_enqueue_style( 'ninja_forms_admin_css', NINJA_FORMS_URL .'/css/ninja_forms_admin.css');
+	wp_enqueue_style( 'ninja_forms_bubblepopup_css', NINJA_FORMS_URL .'/css/jquery.bubblepopup.v2.3.1.css');
+	
+	if(version_compare( $wp_version, '3.3-beta3-19254' , '<')){
+		wp_enqueue_style( 'ninja_forms_tinymce_css', NINJA_FORMS_URL .'/css/editor-buttons.css');
+		wp_admin_css( 'nav-menu' );
+	}else{
+		add_filter('admin_body_class', 'ninja_forms_add_class');
+	}
+}
+
+function ninja_forms_add_class($classes) {
+	// add 'class-name' to the $classes array
+	$classes .= 'nav-menus-php';
+	// return the $classes array
+	return $classes;
+}
+function ninja_form_admin_js(){
+	global $version_compare;
+	$wp_version = get_bloginfo('version');
+	
+	$plugin_settings = get_option("ninja_forms_settings");
+	
+	wp_enqueue_script('jquery-bubblepopup',
+	NINJA_FORMS_URL .'/js/min/jquery.bubblepopup.min.js',
+	array('jquery', 'jquery-ui-core'));	
+	
+	if(version_compare( $wp_version, '3.3-beta3-19254' , '<')){
+		wp_enqueue_script( 'jquery-ui-datepicker', 
+		NINJA_FORMS_URL .'/js/min/jquery.ui.datepicker.min.js',
+		array('jquery', 'jquery-ui-core'));	
+	}
+	wp_enqueue_script( 'jquery-tablesorter', 
+	NINJA_FORMS_URL .'/js/min/jquery.tablesorter.mod.min.js',
+	array('jquery', 'jquery-ui-core'));	
+	
+	wp_enqueue_script( 'jquery-tablesorter-pager', 
+	NINJA_FORMS_URL .'/js/min/jquery.tablesorter.pager.min.js',
+	array('jquery', 'jquery-ui-core'));		
+	
+	if($version_compare){
+		//wp_enqueue_script('ninja_forms_admin_js',
+		//NINJA_FORMS_URL .'/js/ninja_forms_admin.js',
+		//array('jquery', 'jquery-ui-core', 'jquery-ui-sortable', 'jquery-ui-dialog', 'jquery-ui-datepicker'), '', true);		
+		
+		wp_enqueue_script('ninja_forms_admin_js',
+		NINJA_FORMS_URL .'/js/min/ninja_forms_admin.min.js',
+		array('jquery', 'jquery-ui-core', 'jquery-ui-sortable', 'jquery-ui-dialog', 'jquery-ui-datepicker'), '', true);
+	}else{
+		wp_enqueue_script('ninja_forms_admin_js',
+		NINJA_FORMS_URL .'/js/ninja_forms_admin_3.1.js',
+		array('jquery', 'jquery-ui-core', 'jquery-ui-sortable', 'jquery-ui-dialog'), '', true);
+	}
+	wp_localize_script( 'ninja_forms_admin_js', 'settings', array( 'plugin_url' => NINJA_FORMS_URL, 'help_size' => $plugin_settings['help_size'], 'help_color' => $plugin_settings['help_color'], 'admin_help' => $plugin_settings['admin_help']) );
+}
+
+add_action('init', 'ninja_form_display_js');
+function ninja_form_display_js(){
+	global $version_compare;
+	if(isset($_REQUEST['tab'])){
+		$tab = esc_html($_REQUEST['tab']);
+	}else{
+		$tab = '';
+	}
+	if(!is_admin() OR $tab== 'preview' ){
+		
+		wp_enqueue_script('jquery-ui-progressbar',
+		NINJA_FORMS_URL .'/js/min/jquery.ui.progressbar.min.js',
+		array('jquery', 'jquery-ui-core', 'jquery-ui-dialog', 'jquery-form'));				
+		
+		wp_enqueue_script('jquery-bubblepopup',
+		NINJA_FORMS_URL .'/js/min/jquery.bubblepopup.min.js',
+		array('jquery', 'jquery-ui-core', 'jquery-ui-dialog', 'jquery-form'));				
+		
+		wp_enqueue_script('jquery-maskedinput',
+		NINJA_FORMS_URL .'/js/min/jquery.maskedinput.min.js',
+		array('jquery', 'jquery-ui-core', 'jquery-ui-dialog', 'jquery-form'));				
+		
+		wp_enqueue_script('jquery-autonumeric',
+		NINJA_FORMS_URL .'/js/min/autoNumeric.min.js',
+		array('jquery', 'jquery-ui-core', 'jquery-ui-dialog', 'jquery-form'));		
+		
+		if(version_compare( $wp_version, '3.3-beta3-19254' , '<')){
+			wp_enqueue_script('jquery-ui-datepicker',
+			NINJA_FORMS_URL .'/js/min/jquery.ui.datepicker.min.js',
+			array('jquery', 'jquery-ui-core', 'jquery-ui-dialog', 'jquery-form'));		
+			
+			wp_enqueue_script('jquery-ui-widget',
+			NINJA_FORMS_URL .'/js/min/jquery.ui.widget.min.js',
+			array('jquery', 'jquery-ui-core', 'jquery-ui-dialog', 'jquery-form'));
+		}
+		
+		if($version_compare){
+			//wp_enqueue_script('ninja_forms_display-js',
+			//NINJA_FORMS_URL .'/js/min/ninja_forms_display.min.js',
+			//array('jquery', 'jquery-ui-core', 'jquery-ui-dialog', 'jquery-form'));	
+			
+			wp_enqueue_script('ninja_forms_display-js',
+			NINJA_FORMS_URL .'/js/dev/ninja_forms_display.js',
+			array('jquery', 'jquery-ui-core', 'jquery-ui-dialog', 'jquery-form'));			
+		}else{
+			//wp_enqueue_script('ninja_forms_display-js',
+			//NINJA_FORMS_URL .'/js/min/ninja_forms_display_3.1.min.js',
+			//array('jquery', 'jquery-ui-core', 'jquery-ui-dialog', 'jquery-form'));			
+			
+			wp_enqueue_script('ninja_forms_display-js',
+			NINJA_FORMS_URL .'/js/dev/ninja_forms_display_3.1.js',
+			array('jquery', 'jquery-ui-core', 'jquery-ui-dialog', 'jquery-form'));
+			
+			
+		}
+		wp_localize_script( 'ninja_forms_display-js', 'ajax', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
+		wp_localize_script( 'ninja_forms_js-js', 'plugin', array( 'url' => WP_PLUGIN_URL ));
+
+	}
+}
+
+
+add_action('init', 'ninja_form_display_css');
+function ninja_form_display_css(){
+	if(!is_admin()){
+		$plugin_settings = get_option("ninja_forms_settings");
+		$default_style = $plugin_settings['default_style'];
+		if($default_style == 'checked'){
+			wp_enqueue_style( 'jquery-smoothness', NINJA_FORMS_URL .'/css/smoothness/jquery-smoothness.css');
+			wp_enqueue_style( 'ninja_forms_display_css', NINJA_FORMS_URL .'/css/ninja_forms_display.css');
+		}
+		wp_enqueue_style( 'ninja_forms_bubblepopup_css', NINJA_FORMS_URL .'/css/jquery.bubblepopup.v2.3.1.css');
+		
+		if(version_compare( $wp_version, '3.3-beta3-19254' , '<')){
+			wp_enqueue_style( 'ninja_forms_tinymce_css', NINJA_FORMS_URL .'/css/editor-buttons.css');
+		}
+	}
+}
