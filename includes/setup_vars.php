83fe3010c5e5d9b31da0d204628da9fcfbfd15f5
kstover <kstover@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-30 14:16:06 +0000

    Version 1.2.8 Fixes a bug that would cause an email not to be sent on some host setups.
    git-svn-id: https://plugins.svn.wordpress.org/ninja-forms/tags/1.2.8@482343 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/setup_vars.php
@@ -0,0 +1,243 @@
+<?php
+if(isset($_REQUEST['ninja_form_id'])){
+	$form_id = esc_html($_REQUEST['ninja_form_id']);
+}else{
+	$form_id = '';
+}
+if(isset($_REQUEST['tab'])){
+	$current_tab = esc_html($_REQUEST['tab']);
+}else{
+	$current_tab = '';
+}
+$plugin_settings = get_option("ninja_forms_settings");
+
+if(isset($plugin_settings['settings_sidebar_order'])){
+	$settings_sidebar_order = $plugin_settings['settings_sidebar_order'];
+}else{
+	$settings_sidebar_order = '';
+}
+if(isset($plugin_settings['fields_sidebar_order'])){
+	$fields_sidebar_order = $plugin_settings['fields_sidebar_order'];
+}else{
+	$fields_sidebar_order = '';
+}
+if(isset($plugin_settings['subs_sidebar_order'])){
+	$subs_sidebar_order = $plugin_settings['subs_sidebar_order'];
+}else{
+	$subs_sidebar_order = '';
+}
+if(isset($plugin_settings['sub-settings'])){
+	$sub_settings_state = $plugin_settings['sub-settings'];
+}else{
+	$sub_settings_state = '';
+}
+if(isset($plugin_settings['post-settings'])){
+	$post_settings_state = $plugin_settings['post-settings'];
+}else{
+	$post_settings_state = '';
+}
+if(isset($plugin_settings['page-settings'])){
+	$page_settings_state = $plugin_settings['page-settings'];
+}else{
+	$page_settings_state = '';
+}
+if(isset($plugin_settings['predefined-fields'])){
+	$predefined_fields_state = $plugin_settings['predefined-fields'];
+}else{
+	$predefined_fields_state = '';
+}
+if(isset($plugin_settings['cust-fields'])){
+	$cust_fields_state = $plugin_settings['cust-fields'];
+}else{
+	$cust_fields_state = '';
+}
+if(isset($plugin_settings['layout-fields'])){
+	$layout_fields_state = $plugin_settings['layout-fields'];
+}else{
+	$layout_fields_state = '';
+}
+if(isset($plugin_settings['multi-settings'])){
+	$multi_settings_state = $plugin_settings['multi-settings'];
+}else{
+	$multi_settings_state = '';
+}
+if(isset($plugin_settings['post-settings'])){
+	$post_settings_state = $plugin_settings['post-settings'];
+}else{
+	$post_settings_state = '';
+}
+if(isset($plugin_settings['subs-export'])){
+	$subs_export_state = $plugin_settings['subs-export'];
+}else{
+	$subs_export_state = '';
+}
+if(isset($plugin_settings['manage-sub'])){
+	$manage_sub_state = $plugin_settings['manage-sub'];
+}else{
+	$manage_sub_state = '';
+}
+//print_r($plugin_settings);
+
+
+
+if($form_id != 'new'){
+	$ninja_forms_row = $wpdb->get_row( 
+	$wpdb->prepare("SELECT * FROM $ninja_forms_table_name WHERE id = %d", $form_id)
+	, ARRAY_A);
+	$ninja_forms_fields = $wpdb->get_results(
+	$wpdb->prepare( "SELECT * FROM $ninja_forms_fields_table_name WHERE form_id= %d ORDER BY field_order ASC", $form_id)
+	, ARRAY_A);
+	
+	$new_spam = '';
+	$new_submit = '';
+	$new_progressbar = '';
+	$new_steps = '';
+	$new_posttitle = '';
+	$new_postcontent = '';
+	$new_postexcerpt = '';
+	$new_postcat = '';
+	$new_posttags = '';
+	
+	if($ninja_forms_fields){
+		foreach($ninja_forms_fields as $field){
+			switch($field['type']){
+				case 'spam':
+					$new_spam = 'no';
+					break;
+				case 'submit':
+					$new_submit = 'no';
+					break;
+				case 'progressbar':
+					$new_progressbar = 'no';
+					break;
+				case 'steps':
+					$new_steps = 'no';
+					break;
+				case 'posttitle':
+					$new_posttitle = 'no';
+					break;
+				case 'postcontent':
+					$new_postcontent = 'no';
+					break;
+				case 'postexcerpt':
+					$new_postexcerpt = 'no';
+					break;
+				case 'postcat':
+					$new_postcat = 'no';
+					break;				
+				case 'posttags':
+					$new_posttags = 'no';
+					break;
+			}
+		}
+	}
+
+}
+$ninja_all_forms = $wpdb->get_results( 
+$wpdb->prepare( "SELECT * FROM $ninja_forms_table_name ORDER BY title ASC")
+, ARRAY_A);
+
+
+
+if(!$current_tab){
+	$current_tab = 'list';
+}
+if($form_id == 'new'){
+	$current_tab = 'settings';
+	$tab_list = array('settings' => 'Form Settings');
+}else{
+	$tab_list = array("settings" => 'Form Settings',"fields" => 'Form Fields',"preview" => 'Form Preview', 'subs' => 'Form Submissions');					
+}
+
+if($form_id == 'new'){
+	$form_title = 'New Form';
+}else{
+	$form_title = $ninja_forms_row['title'];
+}
+$form_mailto = $ninja_forms_row['mailto'];
+$form_subject = stripslashes($ninja_forms_row['subject']);
+$form_msg = stripslashes($ninja_forms_row['success_msg']);
+$form_desc = stripslashes($ninja_forms_row['desc']);
+$email_msg = stripslashes($ninja_forms_row['email_msg']); 
+$email_from = $ninja_forms_row['email_from'];
+$email_fields = unserialize($ninja_forms_row['email_fields']);
+$show_title = $ninja_forms_row['show_title'];
+$show_desc = $ninja_forms_row['show_desc'];
+$multi = $ninja_forms_row['multi'];
+$ninja_post = $ninja_forms_row['post'];
+$multi_options = $ninja_forms_row['multi_options'];
+if($multi_options){
+	$multi_options = unserialize($multi_options);
+}
+if(isset($multi_options['progress_bar'])){
+	$progress_bar = $multi_options['progress_bar'];
+}else{
+	$progress_bar = '';
+}
+if(isset($multi_options['show_steps'])){
+	$show_steps = $multi_options['show_steps'];
+}else{
+	$show_steps = '';
+}
+if(isset($multi_options['steps_label'])){
+	$steps_label = $multi_options['steps_label'];
+}else{
+	$steps_label = '';
+}
+$previous = $multi_options['previous'];
+$next = $multi_options['next'];
+if(!$next){
+	$next = 'Next';
+}
+if(!$previous){
+	$previous = 'Previous';
+}
+$post_options = $ninja_forms_row['post_options'];
+if($post_options){
+	$post_options = unserialize($post_options);
+}else{
+	$post_options = array();
+}
+if(!isset($post_options['login'])){
+	$post_options['login'] = 'checked';
+}
+if($form_id == 'new'){
+	$form_save_subs = 'checked';
+}else{
+	$form_save_subs = $ninja_forms_row['save_subs'];	
+}
+if($form_id == 'new'){
+	$form_send_email = 'checked';
+}else{
+	$form_send_email = $ninja_forms_row['send_email'];
+}
+$form_landing_page= $ninja_forms_row['landing_page'];
+if($form_id == 'new'){
+	$form_ajax = 'checked';
+}else{
+	$form_ajax = $ninja_forms_row['ajax'];
+}
+$save_status = $ninja_forms_row['save_status'];
+$save_satus_options = unserialize($ninja_forms_row['save_status_options']);
+$save_status_delete = $save_satus_options['delete'];
+$save_status_msg = stripslashes($save_satus_options['msg']);
+$form_append_page = unserialize($ninja_forms_row['append_page']);
+
+
+if($plugin_settings['admin_help'] == 'checked'){
+?>
+<div id="" class="" style="display:none;">
+	<img src="<?php echo NINJA_FORMS_URL;?>/js/jquerybubblepopup-theme/all-black/bottom-left.png">
+	<img src="<?php echo NINJA_FORMS_URL;?>/js/jquerybubblepopup-theme/all-black/bottom-middle.png">
+	<img src="<?php echo NINJA_FORMS_URL;?>/js/jquerybubblepopup-theme/all-black/bottom-right.png">
+	<img src="<?php echo NINJA_FORMS_URL;?>/js/jquerybubblepopup-theme/all-black/middle-left.png">
+	<img src="<?php echo NINJA_FORMS_URL;?>/js/jquerybubblepopup-theme/all-black/middle-right.png">
+	<img src="<?php echo NINJA_FORMS_URL;?>/js/jquerybubblepopup-theme/all-black/tail-left.png">
+	<img src="<?php echo NINJA_FORMS_URL;?>/js/jquerybubblepopup-theme/all-black/tail-top.png">
+	<img src="<?php echo NINJA_FORMS_URL;?>/js/jquerybubblepopup-theme/all-black/top-left.png">
+	<img src="<?php echo NINJA_FORMS_URL;?>/js/jquerybubblepopup-theme/all-black/top-middle.png">
+	<img src="<?php echo NINJA_FORMS_URL;?>/js/jquerybubblepopup-theme/all-black/top-right.png">
+</div>
+<?php
+		}
+		
---
+++ readme.txt
@@ -0,0 +1,97 @@
+=== NinjaForms Lite ===
+Contributors: kstover, jameslaws
+Donate link: http://wpninjas.net
+Tags: form, forms
+Requires at least: 3.1
+Tested up to: 3.3
+Stable tag: 1.2.8
+
+NinjaForms Lite is the free version of NinjaForms Pro. Its drag and drop interface makes it easy to create both simple and complex forms.
+
+== Description ==
+*Update Notice* - Version 1.2.7 contains various security-related fixes. Users are strongly encouraged to download this version to fix any insecurities within Ninja Forms Lite.
+
+NinjaForms Lite is a fully-functional WordPress form creation plugin, with an interface designed to look right at home within your WordPress dashboard.
+
+NinjaForms is a WordPress form building plugin for the rest of us. While there are plenty of other form building plugins available, they tend to be either very expensive or overly complicated. 
+Our guess is you just want something that works without having to take out a second mortgage or fumble through complicated wizards. And you definitely don’t want to have to mess with the code. NinjaForms is the perfect solution.
+
+End users aren’t the only ones who can benefit from NinjaForms simplicity either. Developers can also extend their projects with Ninjas Forms by using it’s convenient hooks and simple structure. 
+And because Ninjas Forms is licensed under the GPL you can use it on all of your projects without any restrictions…forever. Stop wrestling with web forms that are overly complicated and confusing. 
+With NinjaForms, the WordPress Form builder plugin,  you can easily create almost any type of WordPress form you might need and customize it as much or as little as you like.
+
+Some of the features of NinjaForms Lite:
+
+	* Create any type of restricted input field by using our easy to use filters.
+	* Add a spam filter to prevent those pesky bots from filling out your form.
+	* Save form submissions and download them in .xsl format.
+	* Show custom help text for any form field.
+	* Send a completion message to the end-user and to a list of email addresses. You can even customze the email address this comes from.
+	* Use required fields to ensure that your users fill in important information.
+	* Easily create dropdowns, multi-selects, checkboxs, radio buttons, etc. with our drag and drop interface.
+	* Attach a form to a page or post by simply checking a box, or use our shortcodes and functions to place your form anywhere you want.
+	* Fully customizable CSS, make your form look the way you want.
+	
+	
+Upgrade to NinjaForms Pro and get these additional features:
+
+	* Allow users to create posts/pages/etc. from a front-end form.
+	* Make long forms easier to manage by separating them into sections using multi-part forms.
+	* Give users the option to save their progress and come back to complete the form at a later date.
+	* Access to powerful pre and post processing hooks so you can manipulate the form data however you want.
+
+Visit http://www.wpninjas.net/ to check out NinjaForms Pro.
+	
+== Screenshots ==
+
+1. NinjaForms Lite - Field Editing Page
+2. NinjaForms Lite - Form Settings Page
+3. NinjaForms Lite - View Form Submissions Page
+3. NinjaForms Lite - Plugin Settings Page
+
+	
+== Installation ==
+
+Installing NinjaForms Lite is really simple:
+
+1. Upload the plugin folder (i.e. ninja-forms-lite) to the /wp-content/plugins/ directory of your WordPress installation.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. Add or edit your forms by using the new NinjaForms Lite button in your admin sidebar.
+4. Have a snack. You're done.
+
+== Use ==
+
+Using NinjaForms Lite should be fairly straightforward. If you have any questions, see http://www.wpninjas.net for more information.
+
+
+== Advanced Styling ==
+
+NinjaForms gives each form element a set class name, which you can style in your own CSS stylesheet. Additionally, NinjaForms Lite 
+allows you to apply your own custom CSS classes to any form field, so you can easily style your form to your own needs. Again, for more information, please
+visit http://www.wpninjas.net.
+
+
+== Help / Bugs ==
+
+If you need help with NinjaForms Lite, please leave a post in the WordPress.org forum and we will answer it as quickly as we can.
+
+== Requested Features ==
+
+If you have any requests, please post them in the WordPress.org forum.
+
+== Changelog ==
+
+= 1.2.4 =
+* First version of NinjaForms Lite released.
+
+= 1.2.5 =
+* Fixed a bug some users were experiencing with a bad require_once() call.
+
+= 1.2.6 =
+* Fixed some major security-related bugs, please ensure that you are using this version.
+
+= 1.2.7 =
+* Fixed a bug caused by the inability of the programmer to type correctly.
+
+= 1.2.8 =
+* Fixed a bug that would cause the form to not be emailed on some server setups.
